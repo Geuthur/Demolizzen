2d944d75123fc95a2274d8aa790b42ec41dd7af9
Geuthur <geuthur@users.noreply.github.com>
2025-09-10 23:38:10 +0200

    [ADD] Guild settings for future implementations

--- demolizzen/models/guild.py
+++ demolizzen/models/guild.py
@@ -14,3 +14,22 @@ class GuildProfile(models.Model):
 
     def __str__(self):
         return f"{self.guild_name} ({self.guild_id})"
+
+
+class GuildSettings(models.Model):
+    guild = models.OneToOneField(
+        GuildProfile, on_delete=models.CASCADE, related_name="settings"
+    )
+    language = models.CharField(max_length=10, default="en")
+    bank_interest = models.FloatField(default=0.001)  # Interest rate for bank system
+    custom_commands = models.JSONField(null=True)  # JSON field for custom commands
+    disabled_commands = models.JSONField(null=True)  # JSON field for disabled commands
+    disabled_cogs = models.JSONField(null=True)  # JSON field for disabled cogs
+    enabled_cogs = models.JSONField(null=True)  # JSON field for enabled cogs
+
+    class Meta:
+        db_table = "guild_settings"
+        default_permissions = ()
+
+    def __str__(self):
+        return f"Settings for {self.guild.guild_name} ({self.guild.guild_id})"
