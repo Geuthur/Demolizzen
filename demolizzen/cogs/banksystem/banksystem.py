c7fa93620028859385fdfc753d69b0c862ec5279
Geuthur <geuthur@users.noreply.github.com>
2025-09-14 23:12:16 +0200

    [CHANGE] Update Bank System

--- demolizzen/cogs/banksystem/banksystem.py
+++ demolizzen/cogs/banksystem/banksystem.py
@@ -1,3 +1,6 @@
+# Standard Library
+import logging
+
 # Third Party
 from asgiref.sync import sync_to_async
 
@@ -14,6 +17,9 @@ from django.utils import timezone
 # Demolizzen
 from demolizzen.core.bot import Demolizzen
 from demolizzen.models import UserBankAccount, UserProfile
+from demolizzen.models.guild import GuildBankSettings, GuildProfile
+
+logger = logging.getLogger(__name__)
 
 
 class Bank(commands.Cog):
@@ -26,7 +32,6 @@ class Bank(commands.Cog):
         self.last_update_time = timezone.now()
         self.title = "Banksystem"
         self.alias = "bank"
-        self.earns = 0.001
         self.deposits.start()
 
     def cog_unload(self):
@@ -35,6 +40,29 @@ class Bank(commands.Cog):
     bank = SlashCommandGroup(
         "bank", "Banksystem", contexts=[discord.InteractionContextType.guild]
     )
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.debug("Bank Cog is ready.")
+        guild_profiles = [g async for g in GuildProfile.objects.all()]
+        if not guild_profiles:
+            logger.warning(
+                "No guild profiles found. Skipping bank settings initialization."
+            )
+            return
+
+        for guild in guild_profiles:
+            try:
+                create_counter = 0
+                _, created = await GuildBankSettings.objects.aget_or_create(guild=guild)
+                if created:
+                    create_counter += 1
+            except Exception as e:  # pylint: disable=broad-exception-caught
+                logger.exception(
+                    f"Error loading bank settings for guild {guild.guild_name} ({guild.guild_id}): {e}"
+                )
+        if create_counter > 0:
+            logger.info(f"Created bank settings for {create_counter} guild(s).")
 
     # Täglicher Update-Task
     @tasks.loop(hours=168)
@@ -45,49 +73,87 @@ class Bank(commands.Cog):
     @deposits.before_loop
     async def before_deposits(self):
         await self.bot.wait_until_ready()
-        self.bot.logger.info("Bank Interest Ready")
+        logger.info("Bank Interest Ready")
 
     async def process_daily_interest(self):
         try:
-            self.bot.logger.debug("Starting bank interest update...")
-            bank_accounts = [r async for r in UserBankAccount.objects.all()]
-            items = []
-            for account in bank_accounts:
-                interest = int(account.bank * self.earns)
-                account.bank += interest
-                items.append(account)
-            updated = await UserBankAccount.objects.abulk_update(items, fields=["bank"])
-            if updated:
-                self.bot.logger.info(
-                    f"Payout {len(items)} Accounts with {self.earns * 100}% interest."
-                )
-            else:
-                self.bot.logger.error("Failed to update bank accounts.")
+            logger.debug("Starting bank interest update...")
+            guild_bank_settings = [
+                g async for g in GuildBankSettings.objects.all().select_related("guild")
+            ]
+            for guild_bank in guild_bank_settings:
+                bank_accounts = [
+                    r
+                    async for r in UserBankAccount.objects.filter(
+                        user__guild=guild_bank.guild
+                    )
+                ]
+                if bank_accounts and (
+                    guild_bank.last_interest_update is None
+                    or (timezone.now() - guild_bank.last_interest_update).days >= 7
+                ):
+                    items = []
+                    for account in bank_accounts:
+                        interest = int(account.bank * guild_bank.interest_rate)
+                        account.bank += interest
+                        items.append(account)
+                    updated = await UserBankAccount.objects.abulk_update(
+                        items, fields=["bank"]
+                    )
+                    if updated:
+                        logger.info(
+                            f"Payout {len(items)} Accounts with {guild_bank.interest_rate * 100}% interest for {guild_bank.guild}."
+                        )
+                    guild_bank.last_interest_update = timezone.now()
+                    await guild_bank.asave()
         except Exception as e:  # pylint: disable=broad-exception-caught
-            self.bot.logger.exception(f"Error during bank interest update: {e}")
+            logger.exception(f"Error during bank interest update: {e}")
+
+    async def cog_before_invoke(self, ctx: discord.ApplicationContext):
+        user, created = await UserProfile.objects.select_related(
+            "guild"
+        ).aget_or_create(
+            user_id=ctx.author.id,
+            user_name=ctx.author.display_name,
+            guild__guild_id=ctx.guild.id,
+        )
+        ctx.user_profile = user
+        logger.debug(f"User Profile loaded for {ctx.author} in guild {ctx.guild}")
+        if created:
+            logger.debug(
+                f"Created new user profile for {ctx.author} in guild {ctx.guild}"
+            )
+        bank_account, created = await UserBankAccount.objects.aget_or_create(
+            user=user,
+        )
+        if created:
+            logger.debug(
+                f"Created new bank account for user {ctx.author} in guild {ctx.guild}"
+            )
+        ctx.bank_account = bank_account
+        logger.debug(f"Bank Account loaded for {ctx.author} in guild {ctx.guild}")
+        guild_settings, created = await GuildBankSettings.objects.aget_or_create(
+            guild=user.guild,
+        )
+        ctx.guild_settings = guild_settings
+        logger.debug(f"Guild Bank Settings loaded for guild {ctx.guild}")
 
     @bank.command()
     async def create(self, ctx: discord.ApplicationContext):
         """
         Create your Bank Account
         """
-        user, __ = await UserProfile.objects.aget_or_create(
-            user_id=ctx.author.id,
-            user_name=ctx.author.display_name,
-            guild__guild_id=ctx.guild.id,
-        )
-
         try:
-            await UserBankAccount.objects.acreate(user=user)
+            await UserBankAccount.objects.acreate(user=ctx.user_profile)
         except IntegrityError as e:
-            self.bot.logger.debug(f"IntegrityError: {e}")
+            logger.debug(f"IntegrityError: {e}")
             await ctx.respond(
                 f"❌ {ctx.author.mention}, You already have a bank account.",
                 ephemeral=True,
             )
             return
         except Exception as e:  # pylint: disable=broad-exception-caught
-            self.bot.logger.exception(f"Error creating bank account: {e}")
+            logger.exception(f"Error creating bank account: {e}")
             await ctx.respond(
                 "❌ An error occurred while creating your bank account. Please try again later.",
                 ephemeral=True,
@@ -102,20 +168,6 @@ class Bank(commands.Cog):
         """
         Check your money
         """
-        try:
-            user = await UserProfile.objects.select_related("bank_account").aget(
-                user_id=ctx.author.id,
-                user_name=ctx.author.display_name,
-                guild__guild_id=ctx.guild.id,
-            )
-            bank_account = user.bank_account
-        except UserBankAccount.DoesNotExist:
-            await ctx.respond(
-                f"❌ {ctx.author.mention}, You do not have a bank account yet. Please create one using `/bank create`.",
-                ephemeral=True,
-            )
-            return
-
         em = discord.Embed(
             title="",
             color=discord.Color.dark_teal(),
@@ -123,18 +175,15 @@ class Bank(commands.Cog):
         )
         em.add_field(
             name="Personal 💰",
-            value=f"`🪙` **Wallet**: ${bank_account.wallet}",
+            value=f"`🪙` **Wallet**: ${ctx.bank_account.wallet}",
             inline=True,
         )
         em.add_field(
             name="Bank 🏦",
-            value=f"`💵` **Balance:** ${bank_account.bank}\n `📈` **INTEREST:** {self.earns * 100}%",
+            value=f"`💵` **Balance:** ${ctx.bank_account.bank}\n `📈` **INTEREST:** {ctx.guild_settings.interest_rate * 100}%",
             inline=True,
         )
         await ctx.respond(embed=em)
-        self.bot.logger.debug(
-            "Balance checked for user %s in guild %s", ctx.author.id, ctx.guild.id
-        )
 
     @bank.command()
     @option("amount", description="Specify the amount of Coins to Deposit")
@@ -142,23 +191,9 @@ class Bank(commands.Cog):
         """
         Deposit your money to the bank
         """
-        try:
-            user = await UserProfile.objects.select_related("bank_account").aget(
-                user_id=ctx.author.id,
-                user_name=ctx.author.display_name,
-                guild__guild_id=ctx.guild.id,
-            )
-            bank_account = user.bank_account
-        except UserBankAccount.DoesNotExist:
-            await ctx.respond(
-                f"❌ {ctx.author.mention}, You do not have a bank account yet. Please create one using `/bank create`.",
-                ephemeral=True,
-            )
-            return
-
         amount = int(amount)
 
-        if amount > bank_account.wallet:
+        if amount > ctx.bank_account.wallet:
             await ctx.respond("You don't have that much money!")
             return
 
@@ -168,9 +203,9 @@ class Bank(commands.Cog):
 
         # Attempt to update both wallet and bank balances
         def update_balances():
-            bank_account.wallet -= amount
-            bank_account.bank += amount
-            bank_account.save()
+            ctx.bank_account.wallet -= amount
+            ctx.bank_account.bank += amount
+            ctx.bank_account.save()
 
         await sync_to_async(update_balances)()
         await ctx.respond(f"You deposited {amount} :coin:!")
@@ -181,23 +216,9 @@ class Bank(commands.Cog):
         """
         Withdraw your money from the bank
         """
-        try:
-            user = await UserProfile.objects.select_related("bank_account").aget(
-                user_id=ctx.author.id,
-                user_name=ctx.author.display_name,
-                guild__guild_id=ctx.guild.id,
-            )
-            bank_account = user.bank_account
-        except UserBankAccount.DoesNotExist:
-            await ctx.respond(
-                f"❌ {ctx.author.mention}, You do not have a bank account yet. Please create one using `/bank create`.",
-                ephemeral=True,
-            )
-            return
-
         amount = int(amount)
 
-        if amount > bank_account.bank:
+        if amount > ctx.bank_account.bank:
             await ctx.respond("You don't have that much money!")
             return
         if amount < 0:
@@ -207,9 +228,9 @@ class Bank(commands.Cog):
         # Attempt to update both wallet and bank balances
         def update_balances():
             with transaction.atomic():
-                bank_account.bank -= amount
-                bank_account.wallet += amount
-                bank_account.save()
+                ctx.bank_account.bank -= amount
+                ctx.bank_account.wallet += amount
+                ctx.bank_account.save()
 
         await sync_to_async(update_balances)()
         await ctx.respond(f"You Withdraw {amount} :coin:!")
@@ -233,13 +254,9 @@ class Bank(commands.Cog):
             return
 
         try:
-            user_account = await UserProfile.objects.aget(
-                user_id=ctx.author.id, guild__guild_id=server_id
-            )
-            member_account = await UserProfile.objects.aget(
-                user_id=member.id, guild__guild_id=server_id
-            )
-            user_bank_account = user_account.bank_account
+            member_account = await UserProfile.objects.select_related(
+                "bank_account"
+            ).aget(user_id=member.id, guild__guild_id=server_id)
             member_bank_account = member_account.bank_account
         except UserProfile.DoesNotExist:
             await ctx.respond(
@@ -254,10 +271,10 @@ class Bank(commands.Cog):
             )
             return
 
-        if user_account and member_account:
+        if ctx.bank_account and member_account:
             amount = int(amount)
 
-            if amount > user_bank_account.wallet:
+            if amount > ctx.bank_account.wallet:
                 await ctx.respond("You don't have that much money!")
                 return
 
@@ -268,19 +285,19 @@ class Bank(commands.Cog):
             # Update both accounts in the database
             def update_balances():
                 with transaction.atomic():
-                    user_bank_account.wallet -= amount
+                    ctx.bank_account.wallet -= amount
                     member_bank_account.wallet += amount
-                    user_bank_account.save()
+                    ctx.bank_account.save()
                     member_bank_account.save()
 
             await sync_to_async(update_balances)()
             await ctx.respond(f"You gave {amount}:coin:! to {member.mention}")
-        elif user_account and member_account is None:
+        elif ctx.bank_account and member_account is None:
             await ctx.respond(
                 f"❌ {member.mention} does not have a bank account yet. Ask them to create one first.",
                 ephemeral=True,
             )
-        elif user_account is None and member_account:
+        elif ctx.bank_account is None and member_account:
             await ctx.respond(
                 "❌ You does not have a bank account yet. Please create one first.",
                 ephemeral=True,
