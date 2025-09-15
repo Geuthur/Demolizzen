f53443f45413ce1a2694f96cfffa6e3ea1c5a049
Geuthur <geuthur@users.noreply.github.com>
2025-09-15 08:56:21 +0200

    [CHANGE] Update Checks

--- demolizzen/core/checks.py
+++ demolizzen/core/checks.py
@@ -6,14 +6,6 @@ from discord.ext import commands
 from demolizzen.models import GuildProfile
 
 
-async def check_is_owner(ctx: discord.ApplicationContext, precheck=None):
-    if await ctx.bot.is_owner(ctx.author):
-        return True
-    if not precheck:
-        await ctx.respond("You are not the Bot Owner.", ephemeral=True)
-    return False
-
-
 async def check_is_guildowner(ctx: discord.ApplicationContext, precheck=None):
     if ctx.author.id == ctx.guild.owner.id:
         return True
@@ -39,8 +31,10 @@ async def check_is_admin(ctx: discord.ApplicationContext, precheck=None):
     return False
 
 
-async def check_is_mod(ctx: discord.ApplicationContext, precheck=None):
-    if ctx.channel.permissions_for(ctx.author).manage_messages:
+async def check_is_guildmanager(ctx: discord.ApplicationContext, precheck=None):
+    if ctx.author.guild_permissions.manage_guild:
+        return True
+    if await check_is_guildowner(ctx, precheck=True):
         return True
     if await check_is_admin(ctx, precheck=True):
         return True
@@ -50,6 +44,31 @@ async def check_is_mod(ctx: discord.ApplicationContext, precheck=None):
             permissions.append("Guild Owner")
         if await check_is_admin(ctx, precheck=True):
             permissions.append("Admin")
+        permissions.append("Guild Manager")
+        await ctx.respond(
+            "You are not a Guild Manager. Permitted are: " + ", ".join(permissions),
+            ephemeral=True,
+        )
+    return False
+
+
+async def check_is_mod(ctx: discord.ApplicationContext, precheck=None):
+    if ctx.channel.permissions_for(ctx.author).manage_messages:
+        return True
+    if await check_is_guildowner(ctx, precheck=True):
+        return True
+    if await check_is_admin(ctx, precheck=True):
+        return True
+    if await check_is_guildmanager(ctx, precheck=True):
+        return True
+    if not precheck:
+        permissions = []
+        if await check_is_guildowner(ctx, precheck=True):
+            permissions.append("Guild Owner")
+        if await check_is_admin(ctx, precheck=True):
+            permissions.append("Admin")
+        if await check_is_guildmanager(ctx, precheck=True):
+            permissions.append("Guild Manager")
         permissions.append("Moderation")
         await ctx.respond(
             "You are not a Mod. Permitted are: " + ", ".join(permissions),
@@ -87,11 +106,7 @@ async def check_channel(ctx: discord.ApplicationContext):
     return True
 
 
-def is_owner():
-    """Check if the user is the bot owner."""
-    return commands.check(check_is_owner)
-
-
+# Decorators for the checks hierarchy example: is_mod > is_guild_manager > is_admin > is_guild_owner
 def is_guild_owner():
     """Check if the user is the guild owner or higher."""
     return commands.check(check_is_guildowner)
@@ -100,6 +115,11 @@ def is_guild_owner():
 def is_admin():
     """Check if the user is an admin or higher."""
     return commands.check(check_is_admin)
+
+
+def is_guild_manager():
+    """Check if the user is the guild manager or higher."""
+    return commands.check(check_is_guildmanager)
 
 
 def is_mod():
