8ea50fa1a46999f0b150680de8cae02731f4c80f
Geuthur <geuthur@users.noreply.github.com>
2025-09-15 07:53:42 +0200

    [CHANGE] Added `set-interest-rate`  & moved Bank Commands

--- demolizzen/cogs/banksystem/__init__.py
+++ demolizzen/cogs/banksystem/__init__.py
@@ -1,5 +1,12 @@
+# Demolizzen
+from demolizzen.core.bot import Demolizzen
+
 from .banksystem import Bank
+from .banksystem_admin import BankAdmin
+from .banksystem_config import BankConfig
 
 
-def setup(bot):
+def setup(bot: Demolizzen):
+    bot.add_cog(BankConfig(bot))
+    bot.add_cog(BankAdmin(bot))
     bot.add_cog(Bank(bot))
