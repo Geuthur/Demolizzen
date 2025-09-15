f21c97ee5fde02248bd7d8788f46d23073b57aba
Geuthur <geuthur@users.noreply.github.com>
2025-09-15 08:26:16 +0200

    [CHANGE] Update Admin Cog

--- demolizzen/cogs/admin/admin.py
+++ demolizzen/cogs/admin/admin.py
@@ -8,7 +8,6 @@ from discord.ext import commands
 from django.utils import timezone
 
 # Demolizzen
-from demolizzen import models
 from demolizzen.config import EVENTS_SERVER
 from demolizzen.core import checks
 from demolizzen.core.bot import Demolizzen
@@ -26,15 +25,16 @@ class Admin(commands.Cog):
         self.command_ids = {}
 
     admin = SlashCommandGroup(
-        "admin", "Adminsystem", contexts=[discord.InteractionContextType.guild]
+        "admin",
+        "Adminsystem",
+        contexts=[discord.InteractionContextType.guild],
+        default_member_permissions=discord.Permissions(manage_guild=True),
     )
     mod = SlashCommandGroup(
         "moderation",
         "Moderation System",
         contexts=[discord.InteractionContextType.guild],
-    )
-    bank = admin.create_subgroup(
-        "bank", contexts=[discord.InteractionContextType.guild]
+        default_member_permissions=discord.Permissions(manage_messages=True),
     )
     economy = admin.create_subgroup(
         "economy", contexts=[discord.InteractionContextType.guild]
@@ -104,64 +104,6 @@ class Admin(commands.Cog):
             )
         await ctx.respond(embed=embed, ephemeral=True, delete_after=10)
 
-    @mod.command()
-    @commands.guild_only()
-    @checks.is_mod()
-    @option("user", description="Choose User")
-    @option("role", description="Choose Role")
-    async def role(
-        self, ctx: discord.ApplicationContext, user: discord.Member, role: discord.Role
-    ):
-        """Give a Role to a User. Works Hierarchically."""
-
-        if ctx.author.top_role.position < user.top_role.position:
-            return await ctx.respond("You cannot give a role to this user.")
-        try:
-            await user.add_roles(role)
-            await ctx.respond(f"{role} has been added to {user.mention}!")
-            return
-        except discord.HTTPException as e:
-            if e.code == 50013:  # No Permission
-                embed = discord.Embed(
-                    description="❌ I have no permission to do that..."
-                )
-            elif e.code == 50001:  # No Permission
-                embed = discord.Embed(
-                    description="❌ I have no permission to do that..."
-                )
-            self.bot.logger.error(e, exc_info=True)
-            embed = discord.Embed(description="❌ Something went wrong try later.")
-        await ctx.respond(embed=embed, ephemeral=True, delete_after=10)
-
-    @mod.command()
-    @commands.guild_only()
-    @checks.is_mod()
-    @option("user", description="Choose User")
-    @option("derole", description="Choose Role")
-    async def derole(
-        self, ctx: discord.ApplicationContext, user: discord.Member, role: discord.Role
-    ):
-        """Remove a Role from a User."""
-
-        if ctx.author.top_role.position < user.top_role.position:
-            return await ctx.respond("You cannot remove a role from this user.")
-        try:
-            await user.remove_roles(role)
-            await ctx.respond(f"{role} has been removed from {user.mention}!")
-            return
-        except discord.HTTPException as e:
-            if e.code == 50013:  # No Permission
-                embed = discord.Embed(
-                    description="❌ I have no permission to do that..."
-                )
-            elif e.code == 50001:  # No Access
-                embed = discord.Embed(
-                    description="❌ I have no permission to do that..."
-                )
-            self.bot.logger.error(e, exc_info=True)
-            embed = discord.Embed(description="❌ Something went wrong try later.")
-        await ctx.respond(embed=embed, ephemeral=True, delete_after=10)
-
     # ---------------------------- Mission System ----------------------------
     # ---------------------------- Mission System ----------------------------
     # ---------------------------- Mission System ----------------------------
@@ -212,121 +154,4 @@ class Admin(commands.Cog):
 
         EVENTS_SERVER[server_id] = (True, int(amount))
         await ctx.respond(f"🟢 Event has been set to x {amount}.")
-        return
-
-    # ---------------------------- Bank System ----------------------------
-    # ---------------------------- Bank System ----------------------------
-    # ---------------------------- Bank System ----------------------------
-
-    @bank.command(name="give-money")
-    @checks.is_admin()
-    @option("member", description="Choose Member")
-    @option("amount", description="Specify the amount of Coins to Give")
-    async def give_money(
-        self, ctx: discord.ApplicationContext, member: discord.Member, amount: int
-    ):
-        """Give money to a specific user."""
-        amount = int(amount)
-        if amount < 0:
-            await ctx.respond("Amount must be positive!")
-            return
-
-        try:
-            user = await models.UserProfile.objects.select_related("bank_account").aget(
-                user_id=member.id, guild_id=ctx.guild.id
-            )
-            bank_account = user.bank_account
-        except models.UserBankAccount.DoesNotExist:
-            bank_account = await models.UserBankAccount.objects.acreate(
-                user=user,
-            )
-
-        bank_account.wallet += amount
-        # Update the bank account with the new amount
-        await bank_account.asave()
-
-        await ctx.respond(
-            f"You gave {amount}:coin:! to {member.mention}", ephemeral=True
-        )
-        return
-
-    @bank.command(name="remove-money")
-    @checks.is_admin()
-    @option("member", description="Choose Member")
-    @option("amount", description="Specify the amount of Coins to Remove")
-    @option("konto", description="Choose Account", choices=["wallet", "bank"])
-    async def remove_money(
-        self,
-        ctx: discord.ApplicationContext,
-        member: discord.Member,
-        amount: int,
-        konto: str,
-    ):
-        """Remove money from a specific user."""
-        # Get Server-ID for further process
-        server_id = ctx.guild.id
-
-        if amount < 0:
-            await ctx.respond("Amount must be positive!")
-            return
-
-        if konto not in ["wallet", "bank"]:
-            await ctx.respond("Invalid account type selected.")
-            return
-
-        # Fetch the bank account for the member
-        try:
-            user = await models.UserProfile.objects.select_related("bank_account").aget(
-                user_id=member.id, guild_id=server_id
-            )
-            bank_account = user.bank_account
-        except models.UserBankAccount.DoesNotExist:
-            await ctx.respond(f"❌ {member.name}, has no bank account.", ephemeral=True)
-            return
-
-        # If the account exists, check the account type and adjust the balance accordingly
-        if konto == "wallet":
-            amount = min(amount, bank_account.wallet)
-            bank_account.wallet -= amount
-        elif konto == "bank":
-            amount = min(amount, bank_account.bank)
-            bank_account.bank -= amount
-
-        # Update the account in the database
-        await bank_account.asave()
-
-        await ctx.respond(
-            f"You removed {amount}:coin:! from {member.mention}", ephemeral=True
-        )
-        return
-
-    @bank.command(name="remove-bank")
-    @checks.is_admin()
-    @option("member", description="Choose Member")
-    async def reset_money(
-        self, ctx: discord.ApplicationContext, member: discord.Member
-    ):
-        """
-        Reset Bankaccount from Member
-        """
-        # Fetch the bank account for the member
-        try:
-            user = await models.UserProfile.objects.select_related("bank_account").aget(
-                user_id=member.id, guild_id=ctx.guild.id
-            )
-            bank_account = user.bank_account
-        except models.UserBankAccount.DoesNotExist:
-            await ctx.respond(f"❌ {member.name}, has no bank account.", ephemeral=True)
-            return
-
-        # Reset the bank account from the database
-        bank_account.wallet = 0
-        bank_account.bank = 0
-
-        # Update the account in the database
-        await bank_account.asave()
-
-        await ctx.respond(
-            f"Bankaccount from {member.mention} has been reset.", ephemeral=True
-        )
         return
